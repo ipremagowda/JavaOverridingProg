bad9854e1fd55a0dcc0b38ae8aafb32a57827ce9
pm280 <ipremagowda@gmail.com>
2022-02-09 21:07:31 +0530

    Java Programs on Oops Concept

---
+++ src/com/company/javaprog/Demoif.java
@@ -0,0 +1,13 @@
+package com.company.javaprog;
+
+ class DemoIf{
+
+    public static void main(String args[]){
+        int a=10, b=50;
+
+        if(b>a)
+            System.out.println("b is greater");
+    }
+
+}
+
