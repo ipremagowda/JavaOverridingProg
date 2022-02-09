e772c5e7deba2df78f0c4af498698771599d9c10
pm280 <ipremagowda@gmail.com>
2022-02-09 21:37:02 +0530

    Java Programs on Oops Concept

---
+++ src/com/company/javaprog/DemoDoWhile.java
@@ -0,0 +1,12 @@
+package com.company.javaprog;
+
+public class DemoDoWhile
+{
+        public static void main(String args[]) {
+             int n = 3, times = 0;
+             do {
+                System.out.println("Java do while loops:" + n);
+                n++;
+            } while (n <= times);
+        }
+    }
