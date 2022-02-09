06df6885ccaa493689e2982bd8d322b0a19aef03
pm280 <ipremagowda@gmail.com>
2022-02-09 22:10:25 +0530

    Java Programs on Oops Concept

---
+++ src/com/company/javaprog/DemoString.java
@@ -0,0 +1,28 @@
+package com.company.javaprog;
+
+public class DemoString {
+
+        public static void main(String[] args)
+        {
+
+            String s1 = "Hello Java";
+            System.out.println(s1);
+
+            String s2 = new String("Hello Java");
+            System.out.println(s2);
+
+            String str1 = s1.concat(s2);
+            System.out.println(str1);
+                }
+            }
+
+
+
+
+
+
+
+
+
+
+
