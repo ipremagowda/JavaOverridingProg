b48bb4698066744829d1d3fc7637e8a68fd276c9
pm280 <ipremagowda@gmail.com>
2022-02-09 21:09:49 +0530

    Java Programs on Oops Concept

---
+++ src/com/company/javaprog/DemoIfElse.java
@@ -0,0 +1,17 @@
+package com.company.javaprog;
+//If else Program
+public class DemoIfElse {
+        public static void main(String args[]) {
+            int a = 80, b = 30;
+
+            if (b > a)
+            {
+                System.out.println("b is greater");
+            }
+            else
+            {
+                System.out.println("a is greater");
+            }
+        }
+    }
+
