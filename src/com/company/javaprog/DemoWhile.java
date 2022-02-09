4b5d1316c6bca7935668649cb4c3944e988553c5
pm280 <ipremagowda@gmail.com>
2022-02-09 21:23:01 +0530

    Java Programs on Oops Concept

---
+++ src/com/company/javaprog/DemoWhile.java
@@ -0,0 +1,15 @@
+package com.company.javaprog;
+
+public class DemoWhile {
+
+        public static void main(String args[])
+        {
+            int n = 1,  times= 5;
+            while (n <= times)
+            {
+                System.out.println("SAP Commerce Cloud Platform:" + n);
+                n++;
+            }
+        }
+    }
+
