5108fc2097cb26b2edd159d2cd472e397143bdfb
pm280 <ipremagowda@gmail.com>
2022-02-09 21:45:33 +0530

    Java Programs on Oops Concept

---
+++ src/com/company/javaprog/DemoFor.java
@@ -0,0 +1,13 @@
+package com.company.javaprog;
+
+public class DemoFor {
+        public static void main(String args[]) {
+
+             int x = 1, y= 10;
+             for (x= 1; x <= y; x = x + 1)
+             {
+                System.out.println("Java for loops:" + x);
+            }
+        }
+    }
+
