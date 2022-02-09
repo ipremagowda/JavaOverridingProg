bff9b22750d630b9d25ef61525e00569afab2ef5
pm280 <ipremagowda@gmail.com>
2022-02-09 21:12:50 +0530

    Java Programs on Oops Concept

---
+++ src/com/company/javaprog/DemoElseIf.java
@@ -0,0 +1,20 @@
+package com.company.javaprog;
+
+//Else if Program
+
+public class DemoElseIf {
+        public static void main(String args[]) {
+            int a = 5, b = 5;
+
+            if (b > a) {
+                System.out.println("b is greater");
+            }
+            else if(a > b){
+                System.out.println("a is greater");
+            }
+            else {
+                System.out.println("Both are equal");
+            }
+        }
+    }
+
